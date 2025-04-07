d02cd432a635f7d64aa290f04a387f08c18acbe8
RAKESH_MAHAT <81910340+rakeshmahat25@users.noreply.github.com>
2025-04-07 23:33:34 +0545

    Update settings.py
    remove smtp

--- medpredict/settings.py
+++ medpredict/settings.py
@@ -227,8 +227,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "rootking098@gmail.com"
-EMAIL_HOST_PASSWORD = "iqsf glys wizg sjcm"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
